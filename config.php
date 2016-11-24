35c873f21db64880c5e630f3554c32778d61e2d9
hien1832 <hienvip02@gmail.com>
2016-11-24 21:23:57 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 ﻿<?php
 $domain= 'http://trungkien.cf'; // không có / ở cuối
 $linktoken= 'http://trungkien.cf/token.txt'; // nên để trong file txt nhé
-$id_like= '100005310551888'; // ID User VIP
+$id_like= '100014116359726'; // ID User VIP
 $limitlike= '1000000'; // Số Lượng Like/Status
 ?>
